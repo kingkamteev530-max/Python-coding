4f1d46e4162a3e902a6630b80f34d10b8151bae1
kingkamteev530-max <kingkamteev530@gmail.com>
2026-07-27 20:43:30 -0700

    Add files via upload

---
+++ my_daily_routine_printer.py
@@ -0,0 +1,18 @@
+#printing daily routine
+print("My daily routine")
+#printing steps of my routine
+print("Step 1: Wake up")
+print("Step 2: Get ready")
+print("Step 3: Walk to bus stop and go to school")
+print("Step 4: Go to classes")
+print("Step 5: Take the bus home")
+print("Step 6: Chill for an hour")
+print("Step 7: Start homework at 4pm")
+print("Step 8: Eat for 30 minutes")
+print("Step 9: Finsh homework at 9pm")
+print("Step 10: Sleep")
+#printing total time studying
+print("Total study hours in a week")
+print("4.5 hours of studying a day")
+#printing total for 5 days a week
+print(f"Total: {4.5 * 5}")
