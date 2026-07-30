ea59821055c7e4ed8d048f7038bac34442c97b60
kingkamteev530-max <kingkamteev530@gmail.com>
2026-07-29 18:23:47 -0700

    Add files via upload

---
+++ world.py
@@ -0,0 +1,10 @@
+#Print a sentence
+print("welcome to the world of programming!")
+#print a number
+print(4)
+#next line after print
+print("Hello python \n")
+#pass multiple arguments for printing
+print("hello",5 )
+#End argument of print statement
+print("welcome to ", end= "*")
